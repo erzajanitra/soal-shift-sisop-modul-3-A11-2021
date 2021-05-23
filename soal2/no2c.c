fefa20dfb47ea61419be68d9efb44d1f3737038d
Erza Janitradevi <75319371+erzajanitra@users.noreply.github.com>
2021-05-23 11:11:19 +0700

    revisi 2c

--- soal2/no2c.c
+++ soal2/no2c.c
@@ -5,57 +5,66 @@
 #include <sys/wait.h>
 
 int main(){
-	pid_t child_id;
-	int status;
-	int fd1[2];
-	int fd2[2];
-    pipe(fd1);
-    pipe(fd2);
+    pid_t child_id;
+    int status;
+    int fd1[2];
+    int fd2[2];
+    // pipe(fd1);
+    // pipe(fd2);
 
-	
-	if(pipe(fd1) == -1 || pipe(fd2) == -1) {
-		exit(EXIT_FAILURE); 
-	}
-	
-	child_id = fork();
-	if(child_id < 0) {
-		exit(EXIT_FAILURE);
-	}
 
-	if(child_id == 0) {
-        	pid_t child_id2;
-        	int status2;
+    if(pipe(fd1) == -1 || pipe(fd2) == -1) {
+        exit(EXIT_FAILURE); 
+    }
 
-	        child_id2 = fork();
-		
+    child_id = fork();
+    if(child_id < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    if(child_id == 0) {
+            pid_t child_id2;
+            int status2;
+
+            child_id2 = fork();
+
             if(child_id2 < 0) {
-		        exit(EXIT_FAILURE);
-	        }
+                exit(EXIT_FAILURE);
+            }
 
-	        if(child_id2 == 0) {
+            if(child_id2 == 0) {
 
                 close(1);
-		        dup(fd1[1]);
-		        close(fd1[0]);
-		        char *proc[] = {"ps","aux",NULL};
-		        execv("/bin/ps", proc);
-	        } else {
-		        while((wait(&status)) > 0);
-	        	close(0);
-		        dup(fd1[0]);
-		        close(fd1[1]);
+                dup(fd1[1]);
+                close(fd1[0]);
+                close(fd2[0]);
+                close(fd2[0]);
+                char *proc[] = {"ps","aux",NULL};
+                execv("/bin/ps", proc);
+            } else {
+                while((wait(&status2)) > 0);
+                close(0);
+                dup(fd1[0]);
+                close(fd1[1]);
 
-			close(1);
-			dup(fd2[1]);
-			close(fd2[0]);
-				char *head[] = {"head", "-5", NULL};
-				execv("/usr/bin/head", head);
-			}
+                close(1);
+                dup(fd2[1]);
+                close(fd2[0]);
+                char *sort[] = {"sort","-nrk","3,3",NULL};
+                execv("/usr/bin/sort", sort);
+                // char *head[] = {"head", "-5", NULL};
+                // execv("/usr/bin/head", head);
+            }
     }else{
-                	close(0);
-		        dup(fd2[0]);
-		        close(fd2[1]);
-		        char *sort[] = {"sort","-nrk","3,3",NULL};
-		        execv("/usr/bin/sort", sort);
-    }    
+                close(fd1[0]);
+                close(fd1[1]);
+                while((wait(&status)) > 0);
+                close(0);
+                dup(fd2[0]);
+                close(fd2[1]);
+                // char *sort[] = {"sort","-nrk","3,3",NULL};
+                // execv("/usr/bin/sort", sort);
+                char *head[] = {"head", "-5", NULL};
+                execv("/usr/bin/head", head);
+    }
 }
